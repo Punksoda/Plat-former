66ee3f4b28440ba5d220772cb1afb1b719ad1c6c
XPOW <zug3049@gmail.com>
2025-03-08 22:16:45 +0900

    Feat: Player Interaction
    add feature interaction item with using Raycast

---
+++ Assets/Scripts/ItemObject.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public interface IIteractable
+{
+    public string GetInteractPrompt();
+    public void OnInteract();
+}
+public class ItemObject : MonoBehaviour, IIteractable
+{
+    public ItemData data;
+
+    public string GetInteractPrompt()
+    {
+        string str = $"{data.displayName}\n{data.description}";
+        return str ;
+    }
+
+    public void OnInteract()
+    {
+        CharacterManager.Instance.Player.itemData = data;
+        CharacterManager.Instance.Player.addItem?.Invoke();
+        Destroy(gameObject);
+    }
+   
+}
