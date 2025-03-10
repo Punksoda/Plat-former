8aa7073d0e7760e4c79a087d360a8dab262b6a76
XPOW <zug3049@gmail.com>
2025-03-10 10:48:24 +0900

    Add: Condition Special
    to try item consumable

--- Assets/Scripts/UI/PlayerCondition.cs
+++ Assets/Scripts/UI/PlayerCondition.cs
@@ -5,9 +5,9 @@ public class PlayerCondition : MonoBehaviour
     public UICondition uiCondition;
 
     Condition health { get { return uiCondition.health; } }
-
+    Condition special { get { return uiCondition.special; } }
     private void Update()
     {
-        health.Decrease(health.passiveValue * Time.deltaTime);
+        health.Add(health.passiveValue * Time.deltaTime);
     }
 }
--- Assets/Scripts/UI/UICondition.cs
+++ Assets/Scripts/UI/UICondition.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class UICondition : MonoBehaviour
 {
     public Condition health;
+    public Condition special;
    
     void Start()
     {
